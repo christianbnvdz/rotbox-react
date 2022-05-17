bc3f0ce58272cc88f3c1395134f12139d308fa53
Christian Benavidez <christianbenavidez57@gmail.com>
2022-05-17 16:29:59 -0700

    feat: implement register form and styles
    Allow users to register using the register form. Style the form using
    the forms css module. Display whether a user registered successfully or
    if an error occured.

--- src/routes/Register.js
+++ src/routes/Register.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Files() {
+import FormsCSS from '../css/Forms.module.css';
+import RotboxAPI from '../services/api';
+
+function Register() {
+  const [msg, setMsg] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+  const [user, setUserValues] = useState({ username: '', password: '' });
+
+  function register(username, password) {
+    (RotboxAPI.register(username, password)).then((response) => {
+      setMsg('Registered Succesfully');
+      setErrMsg('');
+    }).catch((response) => {
+      setMsg('');
+      setErrMsg(response.response.data.msg);
+    });
+  }
+
+  function updateUserValues(event) {
+    (event.target.id === 'username') ?
+      setUserValues({ username: event.target.value, password: user.password }) :
+      setUserValues({ username: user.username, password: event.target.value });
+  }
+
   return (
-    <h1>Register Component is Working</h1>
+    <section>
+      <h2 className={FormsCSS.title}>Register an Account</h2>
+      <p className={`${FormsCSS.msg} ${FormsCSS.success}`}>{msg}</p>
+      <p className={`${FormsCSS.msg} ${FormsCSS.error}`}>{errMsg}</p>
+      <form className={FormsCSS.form}>
+        <div className={FormsCSS.form_item}>
+          <label for="username">Username</label>
+          <input type="text" id="username" name="username" value={user.username} onChange={updateUserValues} required />
+        </div>
+        <div className={FormsCSS.form_item}>
+          <label for="password">Password</label>
+          <input type="password" id="password" name="password" value={user.password} onChange={updateUserValues} required />
+        </div>
+        <button className={FormsCSS.form_item} type="button" onClick={() => register(user.username, user.password)}>Register</button>
+      </form>
+    </section>
   );
 }
 
-export default Files;
+export default Register;
